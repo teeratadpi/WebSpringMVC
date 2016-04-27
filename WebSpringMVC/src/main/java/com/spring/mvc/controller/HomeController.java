9054d27c42e08c9f30066dc45beb5033eb171b88
Teeratad <Teeratad@iconext.co.th>
2016-04-27 11:22:36 +0700

    delete file not use

--- WebSpringMVC/src/main/java/com/spring/mvc/controller/HomeController.java
+++ WebSpringMVC/src/main/java/com/spring/mvc/controller/HomeController.java
@@ -2,18 +2,12 @@ package com.spring.mvc.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.spring.mvc.repository.CustomerRepository;
-
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private CustomerRepository customerRepository;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
--- WebSpringMVC/src/main/java/com/spring/mvc/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.mvc.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.spring.mvc.model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
-	
-}
